73b61bbbdb83052b3d1eec7fa75014dc5ca3312d
Andris <apogulis@gmail.com>
2023-01-04 18:07:46 +0200

    Deleted comments, code is cleaner now

--- src/test/java/lv/javaguru/demo/finalproject/pages/FinalProjectSelenideTest.java
+++ src/test/java/lv/javaguru/demo/finalproject/pages/FinalProjectSelenideTest.java
@@ -17,28 +17,28 @@ public class FinalProjectSelenideTest {
         $(By.id("CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll")).click();
         Thread.sleep(2500);
 
-        $(By.xpath("(//li[@class='submenu-lvl1__list-item color-theme-17 submenu-lvl1__list-item--has-child']/a)[2]")).click(); //click on Datortehnika
-        $(By.xpath("(//ul[@class='menu product-categories-menu'] /li)[6]")).click(); //click on Datu nesēji
-        $(By.xpath("(//ul[@class='din-list new-cat-list'] /li)[3]")).click(); //click on External Hard Drives
-        $(By.xpath("(//span[@class='catalog-taxons-visual-filter-slider__title'])[4]")).click(); //choose 5 TB hard drives
-        $(By.xpath("//a[@data-value='2p3']")).click(); //choose Western Digital hard drives
+        $(By.xpath("(//li[@class='submenu-lvl1__list-item color-theme-17 submenu-lvl1__list-item--has-child']/a)[2]")).click();
+        $(By.xpath("(//ul[@class='menu product-categories-menu'] /li)[6]")).click();
+        $(By.xpath("(//ul[@class='din-list new-cat-list'] /li)[3]")).click();
+        $(By.xpath("(//span[@class='catalog-taxons-visual-filter-slider__title'])[4]")).click();
+        $(By.xpath("//a[@data-value='2p3']")).click();
         Thread.sleep(2500);
 
-        $(By.xpath("//a[@class='catalog-taxons-product__name' and @data-gtm-link='350551']")).click(); //add hard drive to the shopping cart
-        $(By.xpath("//a[@class='main-button']")).click(); //open shopping cart
-        $(By.xpath("//input[@class='main-button cart-main-button']")).click(); //press continue button
-        $(By.xpath("//form[@id='new_user_guest']//input[@type='email']")).sendKeys("apogulis@gmail.com"); //enter your email address
-        $(By.xpath("//input[@type='submit' and @value='Turpināt']")).click(); //press submit email button
-        $(By.xpath("//input[@name='shipping_unused' and @value='2']")).click(); //choose to receive item in the store
-        $(By.xpath("//input[@name='pickup_point_id' and @value='3543']")).click(); //choose store address
+        $(By.xpath("//a[@class='catalog-taxons-product__name' and @data-gtm-link='350551']")).click();
+        $(By.xpath("//a[@class='main-button']")).click();
+        $(By.xpath("//input[@class='main-button cart-main-button']")).click();
+        $(By.xpath("//form[@id='new_user_guest']//input[@type='email']")).sendKeys("apogulis@gmail.com");
+        $(By.xpath("//input[@type='submit' and @value='Turpināt']")).click();
+        $(By.xpath("//input[@name='shipping_unused' and @value='2']")).click();
+        $(By.xpath("//input[@name='pickup_point_id' and @value='3543']")).click();
 
         $(By.xpath("//input[@id='address_first_name']")).sendKeys("Andris");
         $(By.xpath("//input[@id='address_last_name']")).sendKeys("Pogulis");
         $(By.xpath("//input[@id='address_phone_number']")).sendKeys("28333333");
 
-        $(By.xpath("//button[@class='main-button upcase button-min-width']")).click(); //save your information
-        $(By.xpath("//button[@class='main-button upcase fr small-radius button-min-width checkout-shipping-continue-button']")).click(); //continue to payment
-        $(By.xpath("//input[@id='payment_unused_22']")).click(); //choose pay in store
+        $(By.xpath("//button[@class='main-button upcase button-min-width']")).click();
+        $(By.xpath("//button[@class='main-button upcase fr small-radius button-min-width checkout-shipping-continue-button']")).click();
+        $(By.xpath("//input[@id='payment_unused_22']")).click();
 
         Thread.sleep(6000);
     }
